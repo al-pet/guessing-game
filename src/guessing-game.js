7399fd5447542eed686de65c90d5482ac03cf78d
al-pet <apetrov1024@gmail.com>
2023-02-27 23:28:18 +0300

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0;
+    this.max = 0;
+    this.gues = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.g = 0;
+  }
 
-    }
+  guess() {
+    this.g = Math.round((this.max + this.min) / 2);
+    return this.g;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.g;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.g;
+  }
 }
-
 module.exports = GuessingGame;
